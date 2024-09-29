f6999e5f85c717ccc8eac24cd8e27c6eee6fdfc7
Germo Aabla <germo.aabla.it@gmail.com>
2024-09-29 20:36:15 +0300

    week4

---
+++ Week4WhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberOfTries = 0;
+
+while (numberOfTries < 3)
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    int userGuess = Int32.Parse(Console.ReadLine());
+    //Console.WriteLine(cpuRandom);
+    numberOfTries++;
+
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+}
+Console.WriteLine($"The number of tries: {numberOfTries}");
+Console.WriteLine("Have a nice day!");
